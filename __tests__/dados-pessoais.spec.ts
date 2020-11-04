7ffcf0e37df29e515d249aa4ed6bb6e9f1090dca
isaacmsl <isaacmarlondasilvalourenco@gmail.com>
2020-11-04 17:11:34 -0300

    feat: ++debug nos specs do jest

--- __tests__/dados-pessoais.spec.ts
+++ __tests__/dados-pessoais.spec.ts
@@ -1,4 +1,6 @@
 require('dotenv').config()
+import Debug from 'debug'
+const debug = new Debug('spec:dados-pessoais')
 
 import { SuapiV2 } from '../src/index'
 import { expect } from '@jest/globals'
@@ -7,6 +9,9 @@ const { MATRICULA, SENHA } = process.env
 
 test('deve retornar os dados pessoais', async () => {
     const authToken: string = await SuapiV2.getAuthToken(String(MATRICULA), String(SENHA))
+
     const dadosPessoais = await SuapiV2.getDadosPessoais(authToken)
+    debug(dadosPessoais)
+
     expect(dadosPessoais).toBeDefined()
 })
