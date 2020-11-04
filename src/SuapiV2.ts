7e703b6ccc4a38b555912d94204b77fcfcbd9b52
isaacmsl <isaacmarlondasilvalourenco@gmail.com>
2020-11-04 18:37:05 -0300

    feat: ++getTurmasVirtuais/getTurmaVirtualID

--- src/SuapiV2.ts
+++ src/SuapiV2.ts
@@ -1,10 +1,15 @@
 import axios from 'axios'
 
 import IDadosAlunoV2 from './models/IDadosAlunoV2'
+import ITurmaVirtualLiteV2 from './models/ITurmaVirtualLiteV2'
+import ITurmaVirtualV2 from './models/ITurmaVirtualV2'
 
 export class SuapiV2 {
     public static BASE_URL: string = 'https://suap.ifrn.edu.br/api/v2/'
     public static RESOURCES_DADOS_PESSOAIS_URL: string = 'minhas-informacoes/meus-dados/'
+    public static RESOURCES_TURMAS_VIRTUAIS_URL: string = 'minhas-informacoes/turmas-virtuais'
+    public static RESOURCES_TURMA_VIRTUAL_URL: string = 'minhas-informacoes/turma-virtual'
+
 
     /**
      * 
@@ -36,6 +41,30 @@ export class SuapiV2 {
         try {
             return await SuapiV2.getByAuthorization(
                 SuapiV2.RESOURCES_DADOS_PESSOAIS_URL, 
+                authToken
+            )
+        } catch (error) {
+            throw error
+        }
+    }
+
+    public static async getTurmasVirtuais(authToken: string, ano: number = 2020, periodo: number = 1): Promise<Array<ITurmaVirtualLiteV2>> {
+        try {
+            const TURMAS_VIRTUAIS_URL = `${SuapiV2.RESOURCES_TURMAS_VIRTUAIS_URL}/${ano}/${periodo}`
+            return await SuapiV2.getByAuthorization(
+                TURMAS_VIRTUAIS_URL,
+                authToken
+            )
+        } catch (error) {
+            throw error
+        }
+    }
+
+    public static async getTurmaVirtualID(authToken: string, id: string): Promise<ITurmaVirtualV2> {
+        try {
+            const TURMA_VIRTUAL_URL = `${SuapiV2.RESOURCES_TURMA_VIRTUAL_URL}/${id}`
+            return await SuapiV2.getByAuthorization(
+                TURMA_VIRTUAL_URL,
                 authToken
             )
         } catch (error) {
